59928104e579639409ad8f08db49d1816fa5b5ae
User <user@L450.lan>
2021-05-30 21:12:11 +0800

    Update Amlogic boot script.

---
+++ boot/script/amlogic/mainline/boot_emmc_2.cmd
@@ -0,0 +1,13 @@
+setenv dtb_name "dtb.img"
+setenv dtb_mem_addr "0x1000000"
+setenv kernel_name "uImage"
+setenv kernel_mem_addr "0x1080000"
+setenv condev "console=ttyAML0,115200 console=tty0 consoleblank=0"
+setenv fs_arg_dev "/dev/mmcblk2p2"
+setenv fs_arg_mmc "noinitrd root=${fs_arg_dev} rootfstype=ext4 rootdelay=1 rootflags=data=writeback rw"
+setenv net_arg "net.ifnames=0 mac=${mac}"
+setenv boot_start bootm $kernel_mem_addr - $dtb_mem_addr
+setenv bootargs "${fs_arg_mmc} ${condev} ${net_arg}"
+fatload mmc 2 $dtb_mem_addr $dtb_name
+fatload mmc 2 $kernel_mem_addr $kernel_name
+run boot_start
--- boot/script/q5/mainline/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mkimage -A arm -O linux -T script -C none -a 0 -e 0 -n ‘Execute Boot Script’ -d boot.cmd boot.scr
