527d5f75c878de50811e83a20056fa0d0ca260a5
User <user@L450.lan>
2021-06-05 02:26:40 +0800

    Restructure repository structure.

--- boot/script/m2/boot_emmc.cmd
+++
@@ -1,6 +0,0 @@
-fatload mmc 1:1 0x42000000 zImage
-fatload mmc 1:1 0x43000000 sun7i-a20-m2.dtb
-
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk1p2 rootwait panic=10
-
-bootz 0x42000000 - 0x43000000
--- boot/script/m2/boot_tf.cmd
+++
@@ -1,6 +0,0 @@
-fatload mmc 0:1 0x42000000 zImage
-fatload mmc 0:1 0x43000000 sun7i-a20-m2.dtb
-
-setenv bootargs console=ttyS0,115200 earlyprintk root=/dev/mmcblk0p2 rootwait panic=10
-
-bootz 0x42000000 - 0x43000000
